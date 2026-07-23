11b905ce9a9d9a239eaafbb999f1d913795ddb65
quic-rnamburi <quic-rnamburi@users.noreply.github.com>
2026-07-23 14:41:52 +0000

    deploy: 9be3dba8634416e0b8012da9e576bc14ec51a1f0

---
+++ v0.4/classResource.js
@@ -0,0 +1,5 @@
+var classResource =
+[
+    [ "valueArr", "classResource.html#a24a83fd7ea98135ffc3a2d8fb5e60c3a", null ],
+    [ "values", "classResource.html#adf8c1d9a41a75b6bca752c6d85eba98b", null ]
+];
